b5ae534d66a5d5dab153c01135036516cd8b3cd2
Dnae-ferguson <32795626+Dnae-ferguson@users.noreply.github.com>
2022-03-18 15:23:34 -0400

    Recitation 7

---
+++ charts.py
@@ -0,0 +1,12 @@
+import altair as alt
+from vega_datasets import data
+cars = data.cars()
+
+chart = alt.Chart(cars).mark_point().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin',
+    shape='Origin'
+)
+
+chart.save('chart.html')
